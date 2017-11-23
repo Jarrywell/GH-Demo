229f35a8e4d03b4cee6b49e07a60a4c8bef01329
黎丙炎 <libingyan@meizu.com>
2017-11-23 17:37:54 +0800

    [GHDemo][testjoor]:增加joor的测试demo

---
+++ testjoor/src/main/java/com/android/test/joor/POJOInterface.java
@@ -0,0 +1,18 @@
+package com.android.test.joor;
+
+
+public interface POJOInterface {
+
+    String substring(int beginIndex, int endIndex);
+
+    void tetProxy(String message);
+
+    void setFoo(String foo);
+
+    String getFoo();
+
+    void setBaz(String baz);
+
+    String getBaz();
+
+}
