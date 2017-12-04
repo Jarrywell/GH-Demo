667ff45464a711374b51dc949267b4287efb5de1
黎丙炎 <libingyan@meizu.com>
2017-12-04 21:17:51 +0800

    [GHDemo][Exception]:增加异常测试程序

--- app/src/main/java/com/android/test/demo/MainActivity.java
+++ app/src/main/java/com/android/test/demo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.android.test.demo;
 
+import com.android.test.demo.exception.TestException;
 import com.android.test.joor.TestJOOR;
 import com.android.test.life.TestLife;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TestGH";
@@ -30,5 +34,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), TestLife.class));
             }
         });
+
+        //测试异常
+        testException();
+
+        //测试DAG
+        //TestDAG testDAG = new TestDAG();
+        //testDAG.test1();
+        //testDAG.test2();
+        //testDAG.test3();
+    }
+
+
+    private void testException() {
+        TestException testException = new TestException();
+
+        final String fileName = "/sdcard/img_h5.txt";
+        //final int size = testException.readValueFromFile(fileName);
+        final int size = testException.readValueFromFile(null);
+        Log.d(TAG, "readValueFromFile return size: " + size);
+
+        Map<String, String> map = testException.testFinally2();
+        Log.d(TAG, "testFinally2 map: " + (map != null ? map.get("key") : "null"));
     }
 }
---
+++ app/src/main/java/com/android/test/demo/exception/TestException.java
@@ -0,0 +1,90 @@
+package com.android.test.demo.exception;
+
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestException {
+    private final String TAG = "TestException";
+
+    public int readValueFromFile(String filename) {
+        int size = 0;
+        InputStream in = null;
+        try {
+            Log.d(TAG, "start read file...");
+            if (filename == null){
+                throw new IllegalArgumentException("filename is null");
+            }
+            in = new FileInputStream(filename);
+            int temp;
+            while ((temp = in.read()) != -1) {
+                size++;
+                //Log.d(TAG, "read:" + temp);
+            }
+            Log.d(TAG, "read file size:" + size);
+            return size;
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "FileNotFoundException!!!", e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d(TAG, "IOException!!!", e);
+            e.printStackTrace();
+        } catch (Exception e) {
+            Log.d(TAG, "Exception!!!", e);
+            e.printStackTrace();
+        } finally { //这里会在return之前执行!!!
+            size += 10; //so, 这里会改变正常的返回值吗?
+            Log.d(TAG, "read file finally() size: " + size);
+            if (in != null) {
+                try {
+                    in.close();
+                    in = null;
+                } catch (IOException e) {
+
+                }
+            }
+            //return size; //不要在finally中使用return语句!! 这里的return语句会覆盖try中的return.
+        }
+        return 0;
+    }
+
+    /**
+     * 返回值是2!!! [finally块在retrun语句执行执行之后，返回之前执行]
+     * @return
+     */
+    public int test() {
+        int x = 1;
+        try {
+            ++x;
+            return x;
+        } finally { //finally块在retrun语句执行执行之后，返回之前执行
+            ++x;
+        }
+    }
+
+    /**
+     * 此处涉及到java的传值和传地址区别 ,返回后map不为null, key=finally
+     * @return
+     */
+    public Map<String, String> testFinally2() {
+        Map<String, String> result = new HashMap<>();
+        result.put("key", "start");
+        try {
+            result.put("key", "try");
+            return result;
+        } catch (Exception e) {
+            result.put("key", "catch");
+        } finally {
+            result.put("key", "finally"); //此处生效
+            result = null; //此处不生效
+        }
+        return result;
+    }
+}
